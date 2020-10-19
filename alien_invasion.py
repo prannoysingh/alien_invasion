73a603c6fa54b148aec828d04cafb8f62de6a52f
Prannoy Singh <prannoy.singh@appdynamics.com>
2020-10-18 23:48:45 -0700

    Detecting alien and ship collision,

--- alien_invasion.py
+++ alien_invasion.py
@@ -80,6 +80,18 @@ class AlienInvasion:
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
 
+        self._check_bullet_alien_collision()
+
+    def _check_bullet_alien_collision(self):
+        """Respod to bullet-alien collision"""
+        # Remove any bullets and aliens that have collided.
+        collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
+
+        if not self.aliens:
+            # Destroy existing bullets and create new fleet.
+            self.bullets.empty()
+            self._create_fleet()
+
     def _update_aliens(self):
         """
         Check if the fleet is at an edge,
--- settings.py
+++ settings.py
@@ -13,11 +13,11 @@ class Settings:
         self.ship_speed = 50
 
         # Bullet settings
-        self.bullet_speed = 10.0
-        self.bullet_width = 3
-        self.bullet_height = 15
+        self.bullet_speed = 20.0
+        self.bullet_width = 30
+        self.bullet_height = 25
         self.bullet_color = (60, 60, 60)
-        self.bullets_allowed = 10
+        self.bullets_allowed = 10000
 
         # Alien settings
         self.alien_speed = 1.0
